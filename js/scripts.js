f84014db4f08e34e33266a2e7d945f479b324677
Alvaro Flores and Blake Scala <blake@>
2016-08-08 11:25:56 -0700

    add js converter code

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var gallons = parseInt(prompt("Enter amount of gallons"));
+var liters = (gallons * 3.78541);
+alert(liters);
+
+var liters = parseInt(prompt("Enter amount of liters"));
+var gallons = (liters/3.78541);
+alert(gallons);
